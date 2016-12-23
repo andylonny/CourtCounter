0cb8b7cf2980c42a66c4879d23811bcc53b61086
andylonny <andylonny@gmail.com>
2016-12-23 01:33:05 -0200

    Continous...

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * This method is called when the 3 Points button is clicked.
+     */
+    public void threePoints(View view) {
+        displayForTeamA(3);
+    }
+
+    /**
+     * This method is called when the 2 Points button is clicked.
+     */
+    public void twoPoints(View view) {
+        displayForTeamA(2);
+    }
+
+    /**
+     * This method is called when the Free Throw button is clicked.
+     */
+    public void freeThrow(View view) {
+        displayForTeamA(1);
     }
 }
