b4ee0a14e8240cd67e04fb76755836bc85465aa5
andylonny <andylonny@gmail.com>
2017-03-18 23:02:42 -0300

    Court Counter complete. Portuguese Translated

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.courtcounter;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static com.example.android.courtcounter.R.string.msg;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the 3 Points button is clicked.
      */
     public void addThreeForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 3;
+        scoreTeamA += 3;
         displayForTeamA(scoreTeamA);
     }
 
@@ -36,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the 2 Points button is clicked.
      */
     public void addTwoForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 2;
+        scoreTeamA += 2;
         displayForTeamA(scoreTeamA);
     }
 
@@ -44,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the Free Throw button is clicked.
      */
     public void addOneForTeamA(View v) {
-        scoreTeamA = scoreTeamA + 1;
+        scoreTeamA += 1;
         displayForTeamA(scoreTeamA);
     }
 
@@ -60,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the 3 Points button is clicked.
      */
     public void addThreeForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 3;
+        scoreTeamB += 3;
         displayForTeamB(scoreTeamB);
     }
 
@@ -68,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the 2 Points button is clicked.
      */
     public void addTwoForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 2;
+        scoreTeamB += 2;
         displayForTeamB(scoreTeamB);
     }
 
@@ -76,7 +80,43 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the Free Throw button is clicked.
      */
     public void addOneForTeamB(View v) {
-        scoreTeamB = scoreTeamB + 1;
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * This method is called when the -1 point button is clicked.
+     */
+    public void minusOneForTeamA(View v) {
+        if (scoreTeamA == 0) {
+            Context context = getApplicationContext();
+            CharSequence text = this.getString(R.string.msg);
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+
+        scoreTeamA -= 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * This method is called when the -1 point button is clicked.
+     */
+    public void minusOneForTeamB(View v) {
+        if (scoreTeamB == 0) {
+            Context context = getApplicationContext();
+            CharSequence text = this.getString(R.string.msg);
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+            return;
+        }
+
+        scoreTeamB -= 1;
         displayForTeamB(scoreTeamB);
     }
 
